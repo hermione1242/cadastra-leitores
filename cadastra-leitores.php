9dee19753d89cc4b49a85ba166bb67e9e535ebc5
hermione1242 <victoriafreitas.abreu12@gmail.com>
2024-10-22 18:07:22 -0300

    Add files via upload

--- cadastra-leitores.php
+++ cadastra-leitores.php
@@ -1,41 +1,44 @@
 <?php
 include "config.php";
 if (!$conn){
-    die("falha na conexão{mysqli_connect_error()}");
+die ("Falha na conexão {mysqli_connect_error()}");
+
 } 
-//recebe os dados do formulário
-$nome ='Victoria';
-$dtnasc ='2007-06-12';
-$celular ='(81) 987695395';
-$email = 'victoriafreitas.abreu12@gmail';
-$ra = '1111111';
-$endereco = 'Rua Peixe Agulha';
-$num_end = '13';
-$bairro = 'Ouro Preto';
-$cidade = 'Olinda';
+//receber os dados do formulário
+$nome = 'Bianca';
+$dtnasc = '2007-02-04';
+$celular = '(81) 9990-4508';
+$email = 'siqueirabianca254@gmail.com';
+$ra = '356788';
+$endereco = 'Torres';
+$num_end = '46'; 
+$bairro = 'Janga';
+$cidade = 'Paulista/PE';
 
-//criar variável para inserir o registro
-$sql = '';"INSERT INTO 'leitores'
-('nome', 'DtNasc','Celular', 'Email','RA','Endereco', 'NumEnd','Bairro', 'CidadeUF') VALUES
-('$nome', '$dtnasc','$celular','$email', '$ra','$endereco', '$num_end', '$bairro', '$cidade')";
+// cria variável para inserir o registro
+$sql = "INSERT INTO `leitores`
+( `Nome`, `DtNasc`, `Celular`, `Email`, `RA`, `Endereco`, `NumEnd`, `Bairro`, `CidadeUF`) VALUES
+ ('$nome','$dtnasc','$celular','$email','$ra','$endereco','$num_end','$bairro','$cidade')";
 
- //Executa a consulta SQL. Se falhar, exibe o erro do banco de dados
- $query = mysqli_query(mysql:$conn,query: $sql) or
- die(mysqli_error($conn));
+ //executa a consulta SQL. Se falhar, exibe o erro do banco de dados
+ $query= mysqli_query (mysql:$conn,query: $sql)  or
+ die (mysqli_error (mysql:$conn));
 
+ if ($query){
+echo "<center>";
+echo "Cadastro realizado com sucesso!!<br>";
+echo " <a href = 'index.php'><button title = 'Home page'>Voltar</button></a>";
+echo "</center>";
+ }else {
+echo "<center>";
+echo" <a href = 'index.php'><button title = 'Home page'>Voltar</button></a>";
+echo "</center>";
 
- //Executa a consulta SQL. Se falhar, exibe o erro do banco de dados
- $query = mysqli_query(mysql: $conn,query: $sql) or
- die (mysqli_error($conn));
-
- if($query){
-    echo"<center>";
-    echo"cadrasto realizado com sucesso!!<br>";
-    echo "<a href='index.php'><button title = 'Home page' >voltar</button></a>";
-    echo "</center";
- }else{
-    echo"<center>";
-    echo"<a href= 'index.php'><button title='Home page'>voltar</button></a";
-    echo"</center";
  }
- ?>
+?>
+
+
+
+ 
+
+
